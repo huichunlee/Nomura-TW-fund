f3108f1637fe8da07eb5f314e4eff5cd71dc5f83
Amanda <huichun20@gmail.com>
2017-11-19 23:19:47 +0800

    change date format

---
+++ Robot.R
@@ -0,0 +1,77 @@
+# Robot advisor project
+
+# import libraries
+
+library(zoo)
+library(xts)
+library(dplyr)
+library(quantmod)
+library(PerformanceAnalytics)
+library(readr)
+library(lubridate)
+library(readxl)
+library(ggplot2)
+
+# Set Chinese font family
+
+windowsFonts(BL = windowsFont("微軟正黑體"))
+
+# function: convert data type of char to double but keep Date as char, then tranform to xts object
+
+data2xts <- function(x){
+  Date_temp <- x$Date
+  x$Date <- NULL
+  x <- sapply(x, as.numeric)
+  x <- as.data.frame(x)
+  Date_temp <- as.Date(Date_temp)
+  rownames(x) <- Date_temp
+  x <- as.xts(x)
+  return(x)
+    
+  }
+
+# loading data from Nomura funds
+
+Nomura_Return_All <- read.csv("C:/Users/leeam/Nomura-TW-fund/Nomura_Return_All.csv"
+                                , header=TRUE)
+Nomura_Shape <- read.csv("C:/Users/leeam/Nomura-TW-fund/Nomura_Sharp.csv"
+                                , header=TRUE)
+index_all <- read.csv("C:/Users/leeam/Nomura-TW-fund/index.csv"
+                                , header=TRUE)
+
+# call fuction to convert to xts object
+
+Nomura_Return_All <- data2xts(Nomura_Return_All)
+Nomura_Shape <- data2xts(Nomura_Shape)
+index_all <- data2xts(index_all)
+ 
+# covert % return to numeric return
+
+Nomura_Return_All <- Nomura_Return_All / 100
+index_all <- index_all / 100
+
+# function: calculate cumulative return
+
+cumReturn <- function(x, start_date = "2006-12-29", end_date = "2017-10-31") {
+    period <- paste0(start_date, "/", end_date)
+    result <- Return.cumulative(x[period, ], geometric = TRUE)
+    return(result)
+}
+
+# function: calculate annulized return
+
+annReturn <- function(x, start_date = "2006-12-29", end_date = "2017-10-31" ) {
+    period <- paste0(start_date, "/", end_date)
+    result <- Return.annualized(x[period,], geometric = TRUE)
+    return(result)
+}
+
+# function: calculate periodical return
+# default frequency: "monthly"
+# alternative frequency: "weekly", quarterly", "yearly"
+
+periodReturn <- function(x, frequency = "monthly"){
+    period = paste0("apply.",frequency,"(x, cumReturn)")
+    result <- eval(parse(text = period))
+    return(result)
+}
