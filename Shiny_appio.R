fb021bfd9be2f76a2a933cf285bf2d78ac628825
Amanda <huichun20@gmail.com>
2017-12-27 18:41:44 +0800

    update

--- Shiny_appio.R
+++ Shiny_appio.R
@@ -1,5 +1,4 @@
-install.packages('rsconnect',"C:/Users/leeam/Nomura-TW-fund")
-
+install.packages('rsconnect',"C:/Temp")
 rsconnect::setAccountInfo(name='leeam', token='8E606F8F1EE7EFBC9834D34A563867FD', secret='hWPH6zY/1zwbDJWg4H7MzVXesEZK/ksj686Jabqw')
 
 library(shiny)
